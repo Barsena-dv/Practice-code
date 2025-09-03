4083222379c419d9387d076b9526dcc38c0e2039
Barsena-dv <barsenadhruv1280r@gmail.com>
2025-09-03 22:49:52 +0530

    hooks

---
+++ React.js/myproject-7-usestate-reacthook/src/App.jsx
@@ -0,0 +1,11 @@
+// hooks - special typw of function
+
+import Counter from './Counter.jsx'
+import MyComponent from './MyComponent.jsx'
+function App() {
+  return (
+    <Counter />
+  )
+}
+
+export default App
